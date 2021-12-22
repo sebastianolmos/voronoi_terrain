a262e103ebb4baab69c5768efc6236e8202c5dc4
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-12-22 14:03:42 -0300

    Added dat gui

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.135.0';
+import * as THREE from 'https://cdn.skypack.dev/three@0.135.0'
+import * as dat from 'https://cdn.skypack.dev/dat.gui'
+
+const gui = new dat.GUI()
+const world = {
+    plane: {
+        width: 10,
+        height: 10
+    }
+}
+gui.add(world.plane, "width", 1, 20).onChange(() => {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
+})
+gui.add(world.plane, "height", 1, 20).onChange(() => {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
+})
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -16,11 +34,15 @@ renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
-const planeMaterial = new THREE.MeshBasicMaterial({ 
+const planeMaterial = new THREE.MeshPhongMaterial({ 
     color: 0xff0000,
     side: THREE.DoubleSide})
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
 scene.add(planeMesh)
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 0, 1)
+scene.add(light)
 
 function render(time) {
     time *= 0.001;  // convert time to seconds
