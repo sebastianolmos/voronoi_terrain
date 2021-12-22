82573159229a17370c073d11fd25b0df7469aeff
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-12-22 14:23:09 -0300

    camera controls added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.135.0'
+import {OrbitControls} from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'https://cdn.skypack.dev/dat.gui'
 
 const gui = new dat.GUI()
@@ -8,15 +9,13 @@ const world = {
         height: 10
     }
 }
-gui.add(world.plane, "width", 1, 20).onChange(() => {
-    planeMesh.geometry.dispose()
-    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
-})
-gui.add(world.plane, "height", 1, 20).onChange(() => {
-    planeMesh.geometry.dispose()
-    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
-})
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane)
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane)
 
+function generatePlane() {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -25,13 +24,17 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 5
+camera.up.set(0, 0, 1);
+//camera.lookAt(0, 0, 0);  
 
 const renderer = new THREE.WebGLRenderer()
 
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
+camera.position.z = 5
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
 const planeMaterial = new THREE.MeshPhongMaterial({ 
