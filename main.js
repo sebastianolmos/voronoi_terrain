b8a4d6b58593f20254de9417e273132db0e046f2
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-12-25 18:38:08 -0300

    Voronoi diagram basic implementation

--- main.js
+++ main.js
@@ -4,34 +4,59 @@ import * as dat from 'https://cdn.skypack.dev/dat.gui'
 
 
 function genPoints(minX, maxX, minY, maxY, zCoord) {
-    const particles = 1000;
-    let geometry = new THREE.BufferGeometry();
+    const particles = 1000; // Number of points
+    let pointsGeometry = new THREE.BufferGeometry();
 
-    let positions = [];
-    let colors = [];
-
-    let color = new THREE.Color();
+    let vertices = []; // Array of vertices
     
     let width = maxX - minX;
     let height = maxY - minX;
-    for (let i = 0; i < particles; i++) {
-        let x = minX + Math.random() * width;
-        let y = minY + Math.random() * height;
 
-        positions.push(x, y, zCoord);
+    let voronoi = new Voronoi();
+    let bbox = {xl: minX, xr: maxX, yt: minY, yb: maxY}; 
+    let sites = [];
+
+    // Generate the initial random points
+    for (let i = 0; i < particles; i++) {
+        let x_point = minX + Math.random() * width;
+        let y_point = minY + Math.random() * height;
+
+        vertices.push(x_point, y_point, zCoord);
+        sites.push({x: x_point, y:y_point});
     }
     
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-    geometry.computeBoundingSphere();
+    // Create the geometry of the points
+    pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    pointsGeometry.computeBoundingSphere();
 
-    let material = new THREE.PointsMaterial({
+    // Create the material pf the points
+    let pointsMaterial = new THREE.PointsMaterial({
         size : 0.03,
-        color: 0x000000,
+        color: 0xff0000,
         sizeAttenuation: true
     });
 
-    let p = new THREE.Points(geometry, material);
-    return p;
+    // Create the points Object
+    let p = new THREE.Points(pointsGeometry, pointsMaterial);
+
+    let diagram = voronoi.compute(sites, bbox);
+    console.log(diagram.edges.length);
+
+    const lineMaterial = new THREE.LineBasicMaterial( {color: 0x0000ff } );
+    const linePoints = [];
+
+    for (let i = 0 ; i < diagram.edges.length; i++) {
+        let edge = diagram.edges[i];
+        linePoints.push( new THREE.Vector3(edge.va.x, edge.va.y, zCoord ) );
+        linePoints.push( new THREE.Vector3(edge.vb.x, edge.vb.y, zCoord ) );
+    }
+
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints( linePoints );
+    const l = new THREE.LineSegments( lineGeometry, lineMaterial );
+
+    return {
+        points: p, 
+        lines: l};
 }
 
 function main() {
@@ -77,8 +102,9 @@ function main() {
     const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
     scene.add(planeMesh);
 
-    let points = genPoints(-world.plane.width/2, world.plane.width/2, -world.plane.height/2, world.plane.height/2, 0.001);
+    let {points, lines} = genPoints(-world.plane.width/2, world.plane.width/2, -world.plane.height/2, world.plane.height/2, 0.001);
     scene.add(points);
+    scene.add(lines);
     
     const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(0, 0, 1);
