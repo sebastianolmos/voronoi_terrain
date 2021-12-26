2a4dd3b9d9b80f28e8e36a34bee1828f0087ca6d
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-12-26 15:49:58 -0300

    delaunay and voronoi points render

--- main.js
+++ main.js
@@ -81,9 +81,9 @@ class MapGraph{
         this.buildCenters();
         // Se recorren todas las celdas
         for (let i = 0; i < this.diagram.cells.length; i++) {
-            console.log("Cell ", i);
-            console.log(this.polygons[i].processed);
-            console.log(this.polygons[i]);
+            //console.log("Cell ", i);
+            //console.log(this.polygons[i].processed);
+            //console.log(this.polygons[i]);
             let cell = this.diagram.cells[i];
             // Se recorren los halfedges de la celda
             let heLength = cell.halfedges.length,
@@ -96,7 +96,7 @@ class MapGraph{
             
             // Primera pasada
             for (let j = 0; j < heLength; j++) {
-                console.log("   halfedge ", j);
+                //console.log("   halfedge ", j);
                 let hedge = cell.halfedges[j];
                 let {rigthSite, rightVertex} = this.getRight(hedge);
 
@@ -106,15 +106,15 @@ class MapGraph{
                 if (rigthSite != null &&  
                     this.polygons[rigthSite.voronoiId].processed) 
                 {
-                    console.log("   cell inited");
+                    //console.log("   cell inited");
                     let op_edges = this.polygons[rigthSite.voronoiId].borders;
-                    console.log(op_edges);
+                    //console.log(op_edges);
                     // Recorrer los halfedges del poligono opuesto y encontrar el halfedge opuesto:
                     for (let k = 0; k < op_edges.length; k++) {
                         if (op_edges[k].d1 == this.polygons[i] || op_edges[k].d0 == this.polygons[i]){
                             // Añadir el Edge encontrado
                             edges.push(op_edges[k]);
-                            console.log("----> Edge Founded");
+                            //console.log("----> Edge Founded");
                         }
                     }
                     centers.push(this.polygons[rigthSite.voronoiId])
@@ -149,9 +149,9 @@ class MapGraph{
                         this.polygons[rigthSite.voronoiId].processed) 
                     {   
                         let op_edges = this.polygons[rigthSite.voronoiId].borders;
-                        console.log("Here");
-                        console.log(this.polygons[i] );
-                        console.log(op_edges);
+                        //console.log("Here");
+                        //console.log(this.polygons[i] );
+                        //console.log(op_edges);
                         // Recorrer los halfedges del poligono opuesto y encontrar el halfedge opuesto:
                         for (let k = 0; k < op_edges.length; k++) {
                             if (op_edges[k].d1 == this.polygons[i] || op_edges[k].d0 == this.polygons[i]){
@@ -183,9 +183,9 @@ class MapGraph{
                 // SI el actual fue inicializado y el siguiente no
                 if (edgesReady[j] && !(edgesReady[(j+1)%heLength]) ) {
                     // Al corner+1 se le enlaza el edge siguiente
-                    console.log(j);
-                    console.log(heLength);
-                    console.log(corners);
+                    //console.log(j);
+                    //console.log(heLength);
+                    //console.log(corners);
                     corners[(j+1)%heLength].protrodes.push(edges[(j+1)%heLength]);
                     // Al corner+1 se le enlaza el corner+2
                     corners[(j+1)%heLength].adjent.push(corners[(j+2)%heLength]);
@@ -234,8 +234,8 @@ class MapGraph{
             this.polygons[i].neighbors = centers;
             this.polygons[i].borders = edges;
             this.polygons[i].corners = corners;
-            console.log("End ");
-            console.log(this.polygons[i]);
+            //console.log("End ");
+            //console.log(this.polygons[i]);
             this.polygons[i].processed = true;
 
         }
@@ -375,12 +375,6 @@ function genPoints(minX, maxX, minY, maxY, zCoord) {
 
     let pointsGeometry = new THREE.BufferGeometry();
     const vCenters = voronoiGen.getCenters();
-    let randIdx = Math.floor(Math.random() * voronoiGen.diagram.cells.length);
-    let idx = voronoiGen.diagram.cells[randIdx].halfedges[0].edge.lSite.voronoiId;
-    //console.log(voronoiGen.diagram.cells[randIdx].halfedges[0].edge.va);
-    //console.log(voronoiGen.diagram.cells[idx]);
-    //console.log(voronoiGen.diagram.cells[1]);
-    //console.log(voronoiGen.diagram);
     // Create the geometry of the points
     pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vCenters, 3));
     pointsGeometry.computeBoundingSphere();
@@ -395,14 +389,38 @@ function genPoints(minX, maxX, minY, maxY, zCoord) {
     // Create the points Object
     let p = new THREE.Points(pointsGeometry, pointsMaterial);
 
+    let voronoiGeometry = new THREE.BufferGeometry();
+    let vPoints = [];
+    for (let i = 0; i < mGraph.corners.length; i++){
+        vPoints.push(mGraph.corners[i].x, mGraph.corners[i].y, zCoord);
+    }
+    voronoiGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vPoints, 3));
+    voronoiGeometry.computeBoundingSphere();
+    // Create the material pf the points
+    let voronoiMaterial = new THREE.PointsMaterial({
+        size : 0.03,
+        color: 0x0000FF,
+        sizeAttenuation: true
+    });
 
-    const lineMaterial = new THREE.LineBasicMaterial( {color: 0x0000ff } );
+    // Create the points Object
+    let v = new THREE.Points(voronoiGeometry, voronoiMaterial);
+
+
+    const lineMaterial = new THREE.LineBasicMaterial( {color: 0xFFFFFF } );
+    const delaunayMaterial = new THREE.LineBasicMaterial( {color: 0x000000 } );
+
     const vEdges = [];
-    for (let i = 0; i < mGraph.polygons.length; i++){
-        let poly = mGraph.polygons[i];
-        for (let j = 0; j < poly.borders.length; j++) {
-            vEdges.push(poly.borders[j].v0.x, poly.borders[j].v0.y ,zCoord);
-            vEdges.push(poly.borders[j].v1.x, poly.borders[j].v1.y ,zCoord);
+    const dEdges = [];
+    for (let i = 0; i < mGraph.edges.length; i++){
+        vEdges.push(mGraph.edges[i].v0.x, mGraph.edges[i].v0.y ,zCoord -0.0001);
+        vEdges.push(mGraph.edges[i].v1.x, mGraph.edges[i].v1.y ,zCoord-0.0001);
+
+        let d0 = mGraph.edges[i].d0;
+        let d1 = mGraph.edges[i].d1;
+        if (d0 != null && d1 != null){
+            dEdges.push(mGraph.edges[i].d0.x, mGraph.edges[i].d0.y ,zCoord-0.0001);
+            dEdges.push(mGraph.edges[i].d1.x, mGraph.edges[i].d1.y ,zCoord-0.0001);
         }
         
     }
@@ -411,9 +429,15 @@ function genPoints(minX, maxX, minY, maxY, zCoord) {
     lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vEdges, 3));
     const l = new THREE.LineSegments( lineGeometry, lineMaterial );
 
+    let delaunayGeometry = new THREE.BufferGeometry();
+    delaunayGeometry.setAttribute('position', new THREE.Float32BufferAttribute(dEdges, 3));
+    const d = new THREE.LineSegments( delaunayGeometry, delaunayMaterial );
+
     return {
         points: p, 
-        lines: l};
+        lines: l,
+        delaunay: d,
+        voronoi: v};
 }
 
 function main() {
@@ -455,15 +479,17 @@ function main() {
     console.log(camera);
     
     const planeGeometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10);
-    const planeMaterial = new THREE.MeshPhongMaterial({ 
-        color: 0xFFFFFF,
+    const planeMaterial = new THREE.MeshBasicMaterial({ 
+        color: 0x777777,
         side: THREE.DoubleSide});
     const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
     scene.add(planeMesh);
 
-    let {points, lines} = genPoints(-world.plane.width/2, world.plane.width/2, -world.plane.height/2, world.plane.height/2, 0.001);
+    let {points, lines, delaunay, voronoi} = genPoints(-world.plane.width/2, world.plane.width/2, -world.plane.height/2, world.plane.height/2, 0.001);
     scene.add(points);
     scene.add(lines);
+    scene.add(delaunay);
+    scene.add(voronoi);
     
     const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(0, 0, 1);
