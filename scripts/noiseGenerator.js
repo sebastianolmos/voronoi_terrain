65985aa94e58f3a61f54422fac16bdafd7cc3a81
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-12-28 12:36:44 -0300

    added genNoise function

--- scripts/noiseGenerator.js
+++ scripts/noiseGenerator.js
@@ -9,10 +9,26 @@ export function genNoise(
     noiseScale) 
     {
     let image = [];
+    noise.seed(seed);
     let center = size/2;
     for (let x = 0; x < size; x++) {
         for (let y = 0; y < size; y++) {
             let value;
+            let noiseValue = size / noiseScale;
+            value = (noise.perlin2(x / noiseValue, y / noiseValue) + 1) /2;
+            let dx = center - x,
+          		dy = center - y;   
+			let dist = Math.sqrt(dx*dx +dy*dy); 
+      		let c_value = (dist /(size* radius)) * gradValue;
+			value = Math.min(Math.max((value-c_value), 0.0), 1.0);
+            if (value >= drawheight) {
+				value = 1;
+			} else {
+				value = 0;
+			}
+            let cell = (x + y * size);
+			image[cell] = value;
         }
     }
+    return image;
 }
